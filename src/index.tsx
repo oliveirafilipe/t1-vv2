cf93f07b0bd64bfa071b0418b4ac5025b5544b9f
Filipe Oliveira <ooliveirafilipe@gmail.com>
2022-09-29 15:29:22 -0300

    feat: add drawer skeleton

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,169 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import User from "./application/pages/User";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { Person } from "@mui/icons-material";
+import HomeIcon from "@mui/icons-material/Home";
+import Home from "./application/pages/Home";
+const drawerWidth = 240;
+
+const menu = {
+  home: {
+    icon: HomeIcon,
+    title: "Home",
+    route: "/",
+    component: Home,
+  },
+  user: {
+    icon: Person,
+    title: "Usuário",
+    route: "/user",
+    component: User,
+  },
+};
+
+// Source: https://mui.com/material-ui/react-drawer/#responsive-drawer
+export default function ResponsiveDrawer(props: any) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar />
+      <Divider />
+      <List>
+        {Object.entries(menu).map(([key, content], index) => (
+          <ListItem
+            key={key}
+            // disablePadding
+            component={Link}
+            to={content.route}
+          >
+            {/* <ListItemButton> */}
+            <ListItemIcon>{React.createElement(content.icon)}</ListItemIcon>
+            <ListItemText primary={content.title} />
+            {/* </ListItemButton> */}
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Router>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+            ml: { sm: `${drawerWidth}px` },
+          }}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              Responsive drawer
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box
+          component="nav"
+          sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+          aria-label="mailbox folders"
+        >
+          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: "block", sm: "none" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+          >
+            {drawer}
+          </Drawer>
+          <Drawer
+            variant="permanent"
+            sx={{
+              display: { xs: "none", sm: "block" },
+              "& .MuiDrawer-paper": {
+                boxSizing: "border-box",
+                width: drawerWidth,
+              },
+            }}
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Box>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 3,
+            width: { sm: `calc(100% - ${drawerWidth}px)` },
+          }}
+        >
+          <Toolbar />
+
+          <Routes>
+            {Object.entries(menu).map(([key, content]) => (
+              <Route
+                key={key}
+                path={content.route}
+                element={React.createElement(content.component)}
+              />
+            ))}
+          </Routes>
+        </Box>
+      </Box>
+    </Router>
+  );
+}
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ResponsiveDrawer />
   </React.StrictMode>
 );
