35fd54b831b563c34d24ca3a361cea749efd30c7
arthur-sudbrack-ibarra <arthur.sudbrack.ibarra@hp.com>
2022-10-05 22:00:43 -0300

    Operators page less ugly.

--- src/application/pages/Operators/index.tsx
+++ src/application/pages/Operators/index.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Operator from "../../../domain/models/Operator";
 import { OperatorService } from "../../../domain/services/OperatorService";
 import { OperatorRepository } from "../../../external/repositories/OperatorRepository";
+import "../../styles/styles.css";
 
 export default function Operators() {
   const [name, setName] = useState("");
@@ -25,35 +26,40 @@ export default function Operators() {
   };
   return (
     <>
-      <h1>Cadastrar Operador</h1>
-      <FormControl>
-        <TextField
-          label="Nome"
-          variant="filled"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        ></TextField>
-        <TextField
-          label="Iniciais"
-          variant="filled"
-          value={initials}
-          onChange={(e) => setInitials(e.target.value)}
-        ></TextField>
-        <Button variant="contained" onClick={handleCreateOperator}>
-          Cadastrar
-        </Button>
-      </FormControl>
-      <h1>Operadores Cadastrados</h1>
-      {operators.map((operator) => (
-        <Paper
-          key={operator.id}
-          elevation={2}
-          style={{ padding: "1rem", margin: "1rem" }}
-        >
-          <h2 style={{ margin: 0 }}>Nome: {operator.name}</h2>
-          <h3 style={{ margin: "0.3rem" }}>Iniciais: {operator.initials}</h3>
-        </Paper>
-      ))}
+      <div className="container">
+        <h1>Cadastrar Operador</h1>
+        <FormControl style={{ width: "100%" }}>
+          <TextField
+            style={{ marginBottom: "1rem" }}
+            label="Nome"
+            variant="filled"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></TextField>
+          <TextField
+            label="Iniciais"
+            variant="filled"
+            value={initials}
+            onChange={(e) => setInitials(e.target.value)}
+          ></TextField>
+          <Button variant="contained" onClick={handleCreateOperator}>
+            Cadastrar
+          </Button>
+        </FormControl>
+      </div>
+      <div className="container" style={{ marginTop: "2rem" }}>
+        <h1>Operadores Cadastrados</h1>
+        {operators.map((operator) => (
+          <Paper
+            key={operator.id}
+            elevation={2}
+            style={{ padding: "1rem", margin: "0.4rem", width: "100%" }}
+          >
+            <h2 style={{ margin: 0 }}>Nome: {operator.name}</h2>
+            <h3 style={{ margin: "0.3rem" }}>Iniciais: {operator.initials}</h3>
+          </Paper>
+        ))}
+      </div>
     </>
   );
 }
