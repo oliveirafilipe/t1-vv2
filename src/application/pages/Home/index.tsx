bfeae6a06e3fc9ccced9ae0fb35b012e0556fbbf
Mathias Fatur Kauffmann <mathiaskaufff@gmail.com>
2022-10-11 00:02:47 -0300

    Mudança na página home com o nome da matéria e os integrantes do grupo

--- src/application/pages/Home/index.tsx
+++ src/application/pages/Home/index.tsx
@@ -1,7 +1,18 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+
+const theme = createTheme();
+
 export default function Home() {
   return (
-    <div>
-      <p>Esta é a Home</p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <h1>Trabalho 1 - Verificação e Validação de Software II</h1>
+      <h3>Componentes do grupo: </h3>
+      <h4>Arthur Sudbrack, Filipe Oliveira de Freitas, Mathias Fatur Kauffmann</h4>
+    
+    </ThemeProvider>
   );
 }
