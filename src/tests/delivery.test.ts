6280dfb6acfd6e8b8b963a3955449b6a79cc4008
Mathias Fatur Kauffmann <mathiaskaufff@gmail.com>
2022-10-11 00:29:40 -0300

    Creation of test for saving a delivery

---
+++ src/tests/delivery.test.ts
@@ -0,0 +1,36 @@
+import { expect, jest, test } from "@jest/globals";
+import { DeliveriesService } from "../domain/services/DeliveriesService";
+import { StubDeliveryRepository } from "./stubs/DeliveryRepository.stub";
+
+describe("Delivery Tests", () => {
+  test("Should try to save new delivery", () => {
+    const stubRepository = new StubDeliveryRepository();
+
+    const delivery = {
+      operatorId: "1",
+      date: new Date().toString(),
+      description: "Foo",
+      houseNumber: "12",
+      alreadyCollected: true,
+    };
+
+    const mockSave = jest.fn<typeof stubRepository.save>();
+
+    mockSave.mockImplementation(() => ({ ...delivery, id: "123" }));
+
+    stubRepository.save = mockSave;
+
+    const deliveryService = new DeliveriesService(stubRepository);
+
+    const savedDelivery = deliveryService.save(delivery);
+
+    expect(mockSave).toHaveBeenCalledTimes(1);
+    expect(mockSave).toBeCalledWith(delivery);
+    expect(savedDelivery.id).toBe("123");
+
+  });
+
+
+});
+
+export {};
---
+++ src/tests/stubs/DeliveryRepository.stub.ts
@@ -0,0 +1,23 @@
+import Delivery from "../../domain/models/Delivery";
+import { IDeliveryRepository } from "../../domain/repositories/IDeliveryRepository";
+
+export class StubDeliveryRepository implements IDeliveryRepository {
+  getAll(): Delivery[] {
+    throw new Error("getAll: Method not implemented.");
+  }
+  getAllNotCollected(): Delivery[] {
+    throw new Error("getAllNotCollected: Method not implemented.");
+  }
+  getById(id: string): Delivery {
+    throw new Error("getById: Method not implemented.");
+  }
+  save(delivery: Delivery): Delivery {
+    throw new Error("save: Method not implemented.");
+  }
+  update(delivery: Delivery): Delivery {
+    throw new Error("update: Method not implemented.");
+  }
+  filterByDescription(query: string): Delivery[] {
+    throw new Error("filterByDescription: Method not implemented.");
+  }
+}
