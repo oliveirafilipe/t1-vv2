0a416d920d1dca71c5b8d60648df1ec42c2e3db8
arthur-sudbrack-ibarra <arthur.sudbrack.ibarra@hp.com>
2022-10-11 00:28:10 -0300

    Generate report.

--- src/application/pages/Dashboard/index.tsx
+++ src/application/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import { Paper } from "@mui/material";
+import { Button, Paper } from "@mui/material";
 import { DeliveriesService } from "../../../domain/services/DeliveriesService";
 import { WithdrawnService } from "../../../domain/services/WithdrawnService";
 import { DeliveryRepository } from "../../../external/repositories/DeliveryRepository";
@@ -27,9 +27,31 @@ export default function Dashboard() {
       const date1 = new Date(delivery.date);
       const date2 = new Date(withdrawn.date);
       const daysDiff = Math.abs(date1.getTime() - date2.getTime());
-      days += Math.ceil(daysDiff / (1000 * 3600 * 24));
+      days += daysDiff / (1000 * 3600 * 24);
     });
     return (days / collectedDeliveries.length).toFixed(2).replace(".", ",");
+  };
+  const downloadReport = () => {
+    const csvHeader =
+      "entrega;data-hora;descrição;casa;operador;retirada;morador\n";
+    let csvRows = "";
+    collectedDeliveries.forEach((delivery) => {
+      if (!delivery.id) {
+        return;
+      }
+      const withdrawn = withdrawnService.getByDeliveryId(delivery.id);
+      if (!withdrawn) {
+        return;
+      }
+      csvRows += `${delivery.id};${delivery.date};${delivery.description};${delivery.houseNumber};${withdrawn.operatorId};${withdrawn.date};${withdrawn.residentId}\n`;
+    });
+    const csv = csvHeader + csvRows;
+    const blob = new Blob([csv], { type: "text/csv" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.setAttribute("href", url);
+    link.setAttribute("download", "relatorio.csv");
+    link.click();
   };
   return (
     <div className="container">
@@ -50,6 +72,18 @@ export default function Dashboard() {
           Tempo médio entre registro e retirada de entregas:{" "}
           {getAverageCollectionTime()} dias
         </h3>
+      </Paper>
+      <Paper
+        elevation={2}
+        style={{ padding: "1rem", margin: "0.4rem", width: "100%" }}
+      >
+        <h2 style={{ margin: 0 }}>Relatório</h2>
+        <h3 style={{ margin: "0.3rem" }}>
+          Clique no botão abaixo para gerar o relatório.
+        </h3>
+        <Button variant="contained" onClick={downloadReport}>
+          Gerar Relatório
+        </Button>
       </Paper>
     </div>
   );
