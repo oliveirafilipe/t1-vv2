6cec1fda940ffd9c7a0d954ba97a36315419890e
arthur-sudbrack-ibarra <arthur.sudbrack.ibarra@hp.com>
2022-10-11 00:15:15 -0300

    Dashbord statistics.

--- src/domain/repositories/IWithdrawnRepository.ts
+++ src/domain/repositories/IWithdrawnRepository.ts
@@ -2,5 +2,7 @@ import Withdrawn from "../models/Withdrawn";
 
 export interface IWithdrawnRepository {
   getAll(): Withdrawn[];
+  getLastXDays(days: number): Withdrawn[];
+  getByDeliveryId(deliveryId: string): Withdrawn | undefined;
   save(withdrawn: Withdrawn): Withdrawn;
 }
